6d3b092064c13819529a4095ac31b87c6a06f655
Amin Mohamadi <amindevzone@gmail.com>
2025-07-14 09:48:05 +0330

    Complete slider section

--- resources/views/home/slider.blade.php
+++ resources/views/home/slider.blade.php
@@ -1,93 +1,39 @@
 <section class="slider_section">
+    @php
+        $sliders = App\Models\Slider::all();
+    @endphp
     <div id="customCarousel1" class="carousel slide" data-bs-ride="carousel">
         <div class="carousel-inner">
-            <div class="carousel-item active">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-md-7 col-lg-6">
-                            <div class="detail-box">
-                                <h2 class="mb-3 fw-bold">
-                                    لورم ایپسوم متن
-                                </h2>
-                                <p>
-                                    لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده
-                                    از
-                                    طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و
-                                    سطرآنچنان که
-                                    لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف
-                                    بهبود
-                                    ابزارهای کاربردی می باشد.
-                                </p>
-                                <div class="btn-box">
-                                    <a href="" class="btn1">
-                                        سفارش
-                                    </a>
+            @foreach ($sliders as $slider)
+                <div class="carousel-item {{ $loop->first ? 'active' : '' }} ">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-7 col-lg-6">
+                                <div class="detail-box">
+                                    <h2 class="mb-3 fw-bold">
+                                        {{ $slider->title }}
+                                    </h2>
+                                    <p>
+                                        {{ $slider->body }}
+                                    </p>
+                                    <div class="btn-box">
+                                        <a href="{{ $slider->link_address }}" class="btn1">
+                                            {{ $slider->link_text }}
+                                        </a>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div class="carousel-item">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-md-7 col-lg-6">
-                            <div class="detail-box">
-                                <h2 class="mb-3 fw-bold">
-                                    لورم ایپسوم متن ساختگی
-                                </h2>
-                                <p>
-                                    لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده
-                                    از
-                                    طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و
-                                    سطرآنچنان که
-                                    لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف
-                                    بهبود
-                                    ابزارهای کاربردی می باشد.
-                                </p>
-                                <div class="btn-box">
-                                    <a href="" class="btn1">
-                                        سفارش
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="carousel-item">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-md-7 col-lg-6">
-                            <div class="detail-box">
-                                <h2 class="mb-3 fw-bold">
-                                    لورم ایپسوم متن ساختگی
-                                </h2>
-                                <p>
-                                    لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده
-                                    از
-                                    طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و
-                                    سطرآنچنان که
-                                    لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف
-                                    بهبود
-                                    ابزارهای کاربردی می باشد.
-                                </p>
-                                <div class="btn-box">
-                                    <a href="" class="btn1">
-                                        سفارش
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            @endforeach
         </div>
         <div class="container">
             <ol class="carousel-indicators">
-                <li data-bs-target="#customCarousel1" data-bs-slide-to="0" class="active"></li>
-                <li data-bs-target="#customCarousel1" data-bs-slide-to="1"></li>
-                <li data-bs-target="#customCarousel1" data-bs-slide-to="2"></li>
+                @foreach ($sliders as $slider)
+                    <li data-bs-target="#customCarousel1" data-bs-slide-to="{{ $loop->index }}"
+                        class="{{ $loop->first ? 'active' : '' }}"></li>
+                @endforeach
             </ol>
         </div>
     </div>
